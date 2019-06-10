b4dd4008be9e4b3e51c18603bb2a6e5f96d55b66
RonSala <ron.sala@gmail.com>
2019-06-10 17:11:29 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
     resources :preferences, only: [:index]
   end
 
-  resources :artists, :songs
+  resources :artists do
+    resources :songs, only: %i[index show]
+  end
+
+  resources :songs
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -56,3 +56,11 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+# from https://github.com/thoughtbot/shoulda-matchers/issues/903
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
