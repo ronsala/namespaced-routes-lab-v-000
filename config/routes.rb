2da873f0dc88104d6bae57ee2b75459834884121
RonSala <ron.sala@gmail.com>
2019-06-11 15:41:10 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :admin do
-    resources :preferences, only: [:index]
+    resources :preferences, only: %i[index edit]
   end
 
   resources :artists do
