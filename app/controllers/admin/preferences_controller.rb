b4f2075146d3d78035cf36cc9ded410e34af9ccd
RonSala <ron.sala@gmail.com>
2019-06-11 20:51:27 -0400

    Done.

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -10,6 +10,7 @@
     end
 
     def update
+      # binding.pry
       @preferences = Preference.first
       @preferences.update(song_sort_order: params[:song_sort_order])
       redirect_to admin_preferences_path(@preferences)
